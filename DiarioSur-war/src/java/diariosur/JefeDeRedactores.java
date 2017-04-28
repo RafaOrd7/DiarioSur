399bd71c9972ffb89074b9e0ea2207ed1de7497e
micha <micha@DESKTOP-9DL8292>
2017-04-28 15:21:00 +0200

    JPAs relativas a usuarios modificadas (creados constructores)

--- DiarioSur-war/src/java/diariosur/JefeDeRedactores.java
+++ DiarioSur-war/src/java/diariosur/JefeDeRedactores.java
@@ -18,7 +18,13 @@ import javax.persistence.Entity;
 public class JefeDeRedactores extends Periodista implements Serializable {
 
    
-   
+       public JefeDeRedactores () {
+        
+    }
+    
+    public JefeDeRedactores (String idUser, String nombre, String apellidos, String dni, String email, String password, String empresa, String cargo, String telefono) {
+        super(idUser, nombre, apellidos, dni, email, password, empresa, cargo, telefono);
+    }
 
      @Override
     public int hashCode() {
