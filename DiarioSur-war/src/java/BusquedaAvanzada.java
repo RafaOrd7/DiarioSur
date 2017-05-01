c879eeb7df095445214c909ac92592c001eeca8d
alumnos <alumnos@172.17.215.141>
2017-05-01 17:21:14 +0200

    Merge origin/master into pablo
    Conflicts:
    	DiarioSur-war/nbproject/build-impl.xml
    	DiarioSur-war/nbproject/genfiles.properties
    	DiarioSur-war/nbproject/project.properties
    	DiarioSur-war/src/java/diariosur/Reporte.java
    	DiarioSur-war/web/EnviarReporte.xhtml

---
+++ DiarioSur-war/src/java/BusquedaAvanzada.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author alumnos
+ */
+@Named(value = "busquedaAvanzada")
+@Dependent
+public class BusquedaAvanzada {
+    
+    private String tipo;
+    private int precio;
+    private Date fecha;
+    private String lugar;
+    private String descripcion;
+    private String tags;
+
+    public BusquedaAvanzada() {}
+
+    public String buscar(){
+        return "index.xhtml";
+    }
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the precio
+     */
+    public int getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the lugar
+     */
+    public String getLugar() {
+        return lugar;
+    }
+
+    /**
+     * @param lugar the lugar to set
+     */
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the tags
+     */
+    public String getTags() {
+        return tags;
+    }
+
+    /**
+     * @param tags the tags to set
+     */
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+}
